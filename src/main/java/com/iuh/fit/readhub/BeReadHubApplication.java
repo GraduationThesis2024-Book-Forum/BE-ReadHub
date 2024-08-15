6d165f7d97632e833311bf4624d41666803f4304
Anh Thu <thuleanh543@gmail.com>
2024-08-15 17:25:34 +0700

    SettingUpMySQLContainer

---
+++ src/main/java/com/iuh/fit/readhub/BeReadHubApplication.java
@@ -0,0 +1,13 @@
+package com.iuh.fit.readhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeReadHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeReadHubApplication.class, args);
+	}
+
+}
