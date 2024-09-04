a62e1b5590bbdbf6895f16e5808b75331724ce5b
NguyenDucChien <cnchien@gmail.com>
2024-09-04 23:37:57 +0700

    update: create dto RegistrationResponse

--- src/main/java/com/iuh/fit/readhub/controllers/AuthenController.java
+++ src/main/java/com/iuh/fit/readhub/controllers/AuthenController.java
@@ -1,10 +1,31 @@
 package com.iuh.fit.readhub.controllers;
 
+import com.iuh.fit.readhub.dto.RegistrationResponse;
+import com.iuh.fit.readhub.services.AuthenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/authen")
 public class AuthenController {
+    @Autowired
+    private AuthenService authService;
 
+    @PostMapping("/register")
+    public ResponseEntity<RegistrationResponse> registerForUser(@RequestParam String email, @RequestParam String username, @RequestParam String password) {
+        RegistrationResponse response = authService.registerForUser(email, username, password);
+
+        if (response.isSuccess()) {
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } else if (response.getMessage().equals("Email đã được đăng ký")) {
+            return new ResponseEntity<>(response, HttpStatus.CONFLICT);
+        } else {
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        }
+    }
 }
--- src/main/java/com/iuh/fit/readhub/services/AuthenService.java
+++ src/main/java/com/iuh/fit/readhub/services/AuthenService.java
@@ -1,6 +1,8 @@
 package com.iuh.fit.readhub.services;
 
 
+import com.iuh.fit.readhub.constants.ValidationConstants;
+import com.iuh.fit.readhub.dto.RegistrationResponse;
 import com.iuh.fit.readhub.models.User;
 import com.iuh.fit.readhub.models.UserRole;
 import com.iuh.fit.readhub.repositories.UserRepository;
@@ -9,6 +11,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.regex.Pattern;
 
 @Service
 public class AuthenService {
@@ -18,10 +21,18 @@ public class AuthenService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    public String registerForUser(String email, String username, String password) {
-//        if (userRepository.existsByEmail(email)) {
-//            throw new IllegalStateException("Email đã được đăng ký");
-//        }
+    public RegistrationResponse registerForUser(String email, String username, String password) {
+        if (userRepository.existsByEmail(email)) {
+            return new RegistrationResponse(false, "Email đã được đăng ký");
+        }
+
+        if (!Pattern.matches(ValidationConstants.EMAIL_REGEX, email)) {
+            return new RegistrationResponse(false, "Email không hợp lệ");
+        }
+
+        if (!Pattern.matches(ValidationConstants.PASSWORD_REGEX, password)) {
+            return new RegistrationResponse(false, "Mật khẩu phải chứa ít nhất 8 ký tự, bao gồm chữ cái, chữ số và ký tự đặc biệt");
+        }
 
         String encodedPassword = passwordEncoder.encode(password);
         User newUser = new User();
@@ -33,6 +44,7 @@ public class AuthenService {
 
         userRepository.save(newUser);
 
-        return "Đăng ký thành công!";
+        return new RegistrationResponse(true, "Đăng ký thành công!");
     }
+
 }
