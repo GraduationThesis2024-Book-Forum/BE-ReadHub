a2939a19d01e3b0ae47a8339e20b091434924bec
NguyenDucChien <cnchien@gmail.com>
2024-11-17 00:44:12 +0700

    update: remove DiscussionRepository

--- src/main/java/com/iuh/fit/readhub/repositories/DiscussionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.iuh.fit.readhub.repositories;
-
-import com.iuh.fit.readhub.models.Discussion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DiscussionRepository extends JpaRepository<Discussion, Long> {
-}
