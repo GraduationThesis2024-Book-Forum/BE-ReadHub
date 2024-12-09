a6f0aee9c05714f6468afca7f0c79c3a6ae4d1c8
NguyenDucChien <cnchien@gmail.com>
2024-12-09 21:28:03 +0700

    update: REGISTER_SUCCESS("Register Success!"),

--- src/main/java/com/iuh/fit/readhub/constants/ValidationMessages.java
+++ src/main/java/com/iuh/fit/readhub/constants/ValidationMessages.java
@@ -8,7 +8,7 @@ public enum ValidationMessages {
     EMAIL_INVALID("Email không hợp lệ"),
     PASSWORD_INVALID("Password phải chứa ít nhất 8 ký tự, bao gồm chữ cái, chữ số và ký tự đặc biệt"),
     USERNAME_INVALID("Username phải có ít nhất 6 ký tự"),
-    REGISTER_SUCCESS("Đăng ký thành công!"),
+    REGISTER_SUCCESS("Register Success!"),
     USERNAME_ALREADY_EXISTS("Username đã tồn tại"),
     OTP_SENT("Mã OTP đã được gửi đến email của bạn"),
     OTP_NOT_EMPTY("Mã OTP không được để trống"),
